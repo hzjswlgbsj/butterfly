851ec1fafa1f575f721bb1625b6774911a3cc444
Sixty <hiliulin@aliyun.com>
2023-06-27 23:49:03 +0800

    feat: 增加toolbar的基础代码

---
+++ shims-other.d.ts
@@ -0,0 +1,32 @@
+declare module "*.less" {
+  const less: any;
+  export default less;
+}
+declare module "*.md" {
+  const md: any;
+  export default md;
+}
+declare module "*.css" {
+  const css: any;
+  export default css;
+}
+declare module "*.svg" {
+  const svg: any;
+  export default svg;
+}
+declare module "*.csvg" {
+  const svg: any;
+  export default svg;
+}
+declare module "*.png" {
+  const png: any;
+  export default png;
+}
+
+declare module "*.woff";
+declare module "*.woff2";
+
+declare module "mxgraph" {
+  const mxgraph: any;
+  export default mxgraph;
+}

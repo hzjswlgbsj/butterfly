1186d4b8d4f8e311a324b13f23559c34737949de
Sixty <hiliulin@aliyun.com>
2023-07-08 23:50:40 +0800

    feat: 下划线

---
+++ packages/toolbar/core/icons/Underline.tsx
@@ -0,0 +1,13 @@
+import { memo } from 'react';
+
+const Undo = memo(() => {
+  return (
+    <div id='toolbar-button-undo'>
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M18 18.3v1.3H5v-1.3h13zM8.4 5v7.5a3.1 3.1 0 002.924 3.095l.176.005a3.1 3.1 0 003.095-2.924l.005-.176V5H16v7.5a4.5 4.5 0 11-9 0V5h1.4z" fill="#454D5A"></path>
+      </svg>
+    </div>
+  )
+})
+
+export default Undo

cc270f4f3b889409cf3a372deea860773c42e5f0
Sixty <hiliulin@aliyun.com>
2023-07-28 10:01:39 +0800

    feat: 修改行内code和引用块的样式

---
+++ packages/editor/components/Element/style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Code = styled.code`
+  background-color: #f1f1f1;
+  padding: 0.15em 0.5em;
+  border-radius: 4px;
+  color: #476582;
+  transition: color 0.5s, background-color 0.5s;
+`;
+
+export const BlockquoteWrapper = styled.blockquote`
+  margin: 1rem 0;
+  border-left: 0.2rem solid rgba(60, 60, 60, 0.29);
+  padding-left: 1rem;
+  transition: border-color 0.5s;
+`;
+export const Blockquote = styled.p`
+  margin: 0;
+  font-size: 16px;
+  color: rgba(60, 60, 60, 0.7);
+  transition: color 0.5s;
+`;
